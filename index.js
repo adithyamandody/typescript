9e6cf649454d42d6401f4f0a6a12c0cd51592ec0
adithya <madithyam16@gmail.com>
2022-11-10 16:10:25 +0530

    second push

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("hello world");
+//explicit types
+var firstnumber = 50;
+var x = ["hello", 10];
+//enums
+var continents;
+(function (continents) {
+    continents[continents["North_america"] = 0] = "North_america";
+    continents[continents["South_america"] = 1] = "South_america";
+    continents[continents["Africa"] = 2] = "Africa";
+    continents[continents["Europe"] = 3] = "Europe";
+    continents[continents["Antartica"] = 4] = "Antartica";
+    continents[continents["Australia"] = 5] = "Australia";
+})(continents || (continents = {}));
+;
+var region = continents.Africa; //2
--- index.ts
+++ index.ts
@@ -1,1 +1,22 @@
 console.log("hello world");
+
+//explicit types
+let firstnumber:number=50;
+
+
+//Tuple
+type stringAndNumber=[string,number];
+let x:stringAndNumber=["hello",10];
+
+//enums
+
+enum continents {
+    North_america,
+    South_america,
+    Africa,
+    Europe,
+    Antartica,
+    Australia
+} ;
+
+var region= continents.Africa; //2
